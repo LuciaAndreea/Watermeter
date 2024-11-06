57614e60238b20826b9b06ed93268c4ea486fbd9
Lucia <luciaandreeaursu@yahoo.com>
2024-11-06 12:28:50 +0200

    create a method to add a reading and to do all the necessary checks before doing it

--- Watermeter.java
+++ Watermeter.java
@@ -31,4 +31,36 @@ public class Watermeter {
     private int yearIndex(int year){
         return year - 2023; //the year the reading starts
     }
+
+    public void addReading(int year , String month, int coldWaterReading, int warmWaterReading){
+        month = month.toLowerCase();
+        int indexYear = yearIndex(year);
+        Integer monthIndex = months.get(month);
+
+        //check if the month is valid
+        if(indexYear <0 || indexYear >=coldWater.length || monthIndex ==null){
+            System.out.println("The month is not valid.");
+            return;
+        }
+
+        //check if the reading already exists
+        if(coldWater[indexYear][monthIndex] !=0 || warmWater[indexYear][monthIndex] !=0){
+            System.out.println("The reading for "+month+" "+year+" already exists.");
+        }
+
+        //check if the values of readings are higher than those oe month ago
+        if(indexYear >0 || monthIndex>0){
+            int previousYearIndex = monthIndex == 0? indexYear-1 : indexYear;
+            int previousMonthIndex = monthIndex ==0? 11 : monthIndex-1;
+            if(coldWaterReading < coldWater[indexYear][monthIndex] || warmWaterReading < warmWater[indexYear][monthIndex]){
+                System.out.println("The values of watermeters should be higher than last month.");
+                return;
+            }
+        }
+
+        //if all the checking are good add the readings in coldWater and warmWater
+        coldWater[indexYear][monthIndex] = coldWaterReading;
+        warmWater[indexYear][monthIndex] = warmWaterReading;
+        System.out.println("The reading for "+month+" "+year+" has been added.");
+    }
 }
